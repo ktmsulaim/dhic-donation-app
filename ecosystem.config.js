3334ed284cfeaef2e297b0c1a9bb7b9dede0d68a
ktmsulaim <ktmsulaim@gmail.com>
2022-05-29 21:29:58 +0530

    Configured axios base url in prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,18 @@
 module.exports = {
     apps: [
-      {
-        name: 'DonationApp',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-      }
+        {
+            name: 'DonationApp',
+            exec_mode: 'cluster',
+            instances: 'max', // Or a number of instances
+            script: './node_modules/nuxt/bin/nuxt.js',
+            args: 'start',
+            port: process.env === 'production' ? 3003 : 3000,
+            env: {
+                NODE_ENV: 'development'
+            },
+            env_production: {
+                NODE_ENV: 'production'
+            }
+        }
     ]
-  }
+}
